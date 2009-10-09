34e54b492fb079ace7d528f3a972f403351d09eb
Tyson Avery <tysonaverydfc@gmail.com>
2009-10-09 15:22:27 +1030

    commit recent changes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  # Each location has a channel
+  # Each location has a channel and a state
   belongs_to :channel
   belongs_to :state
 end
--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,6 +1,9 @@
 class ProductType < ActiveRecord::Base
-validates_inclusion_of :first_production,
+  has_many :products
+
+  validates_inclusion_of :first_production,
     :in => Date.civil(2004,1,1) .. Date.today,
 	:message => "Must be a valid production date!"
 
+	
 end
